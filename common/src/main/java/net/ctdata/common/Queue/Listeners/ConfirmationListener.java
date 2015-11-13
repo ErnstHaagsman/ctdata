d3db6ce820982c0f575ae4d16a75312603cde7bb
Ernst Haagsman <ernsthaagsman@gmail.com>
2015-11-12 23:48:57 -0800

    Fix dumb mistakes in ConfirmationListener
    I should pay more attention when copy-pasting stuff

--- common/src/main/java/net/ctdata/common/Queue/Listeners/ConfirmationListener.java
+++ common/src/main/java/net/ctdata/common/Queue/Listeners/ConfirmationListener.java
@@ -9,18 +9,18 @@ public abstract class ConfirmationListener extends AbstractQueueListener<Confirm
     String routingKey = "datapoints.confirm.*.*";
 
     /**
-     * Listens for all incoming observations on the network
+     * Listens for all incoming confirmations on the network
      */
     public ConfirmationListener() {
         super(Confirmation.class);
     }
 
     /**
-     * Listens for incoming observations from the specified sensor
+     * Listens for incoming confirmations from the specified sensor
      */
     public ConfirmationListener(UUID raspberryNodeId, int sensor) {
         super(Confirmation.class);
-        routingKey = String.format("datapoints.incoming.%s.%d", raspberryNodeId, sensor);
+        routingKey = String.format("datapoints.confirm.%s.%d", raspberryNodeId, sensor);
     }
 
     @Override
