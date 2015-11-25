43282daab10e0e048b76ecec5b950404b8d263ae
shahnimesh2 <shahnimesh2@yahoo.co.in>
2015-11-25 11:26:46 -0800

    web UI

--- webapp/src/main/java/net/ctdata/webapp/controllers/Greeting.java
+++ webapp/src/main/java/net/ctdata/webapp/controllers/Greeting.java
@@ -3,5 +3,26 @@ package net.ctdata.webapp.controllers;
 /**
  * Created by Nimesh on 11/25/2015.
  */
+
 public class Greeting {
+
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
 }
