f97d7677d33c43e4429ea7375b91830275feb690
Ernst Haagsman <ernsthaagsman@gmail.com>
2015-12-08 14:49:11 -0800

    Upload all branches to S3

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == 'master' ]]; then
 
-    curl -sL https://raw.githubusercontent.com/travis-ci/artifacts/master/install | bash
+curl -sL https://raw.githubusercontent.com/travis-ci/artifacts/master/install | bash
 
-    mkdir jars
-    mv datanode/build/libs/*.jar ./jars/
-    mv orchestrator/build/libs/*.jar ./jars/
-    mv raspnodesim/build/libs/*.jar ./jars/
-    mv sensorgateway/build/libs/*.jar ./jars/
-    mv webapp/build/libs/*.jar ./jars/
+mkdir jars
+mv datanode/build/libs/*.jar ./jars/
+mv orchestrator/build/libs/*.jar ./jars/
+mv raspnodesim/build/libs/*.jar ./jars/
+mv sensorgateway/build/libs/*.jar ./jars/
+mv webapp/build/libs/*.jar ./jars/
 
-    zip jars.zip ./jars/*.jar
+zip ${TRAVIS_BRANCH}.zip ./jars/*.jar
 
-    artifacts upload \
-        --s3-region us-west-1 \
-        --target-paths build \
-        --permissions public-read \
-        jars.zip
+artifacts upload \
+    --s3-region us-west-1 \
+    --target-paths build \
+    --permissions public-read \
+    ${TRAVIS_BRANCH}.zip
 
-fi
